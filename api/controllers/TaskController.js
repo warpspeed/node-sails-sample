842031070fa03bfc7e1c742b22039aee99235823
Keenan Treat <keet@sas.upenn.edu>
2015-06-16 12:03:29 -0500

    Added favicon, modified Task model name, altered MongoDB connection

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -1,20 +1,20 @@
 module.exports = {
 	all: function(req, res) {
-		Tasks.find().sort({ _id : -1 }).exec(function createCB (err, tasks) {
+		Task.find().sort({ _id : -1 }).exec(function createCB (err, tasks) {
 			return res.view('index', {tasks: tasks});
 		});
 	},
 
 	create: function(req, res) {
 		var name = req.param('name');
-		if(req.param('name') == '') {
+		// var now  = Date().
+		if(!req.param('name')) {
 			return res.redirect('/')
 		}
 
-		Tasks.create({name: name}).exec(function createCB(err, created){
+		Task.create({name: name}).exec(function createCB(err, created){
 
 			if(err) { return res.json({error: err})}
-			console.log('Created user with the name' + created.name);
 			return res.redirect('/');
 		});
 	},
@@ -22,8 +22,8 @@ module.exports = {
 	toggleComplete: function(req, res) {
 		var id = req.params.id;
 
-		Tasks.findOne(id).exec(function createCB(err, task) {
-			Tasks.update(task.id, {is_complete: !task.is_complete}).exec(function createCB(err, updated) {
+		Task.findOne(id).exec(function createCB(err, task) {
+			Task.update(task.id, {is_complete: !task.is_complete}).exec(function createCB(err, updated) {
 				if(err) { console.log(err); }
 				return res.redirect('/');
 			});
@@ -31,9 +31,8 @@ module.exports = {
 	},
 
 	clearComplete: function(req, res) {
-		Tasks.destroy({is_complete: true}).exec(function createCB(err, cleared) {
+		Task.destroy({is_complete: true}).exec(function createCB(err, cleared) {
 			if (err) { return res.json({error: err}) }
-
 			return res.redirect('/');
 		})
 	}
